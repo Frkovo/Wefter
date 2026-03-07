d986ae81acdd4cad5db3173ad6be7977ee6f9de6
ICSUCS <owenxuaozai@163.com>
2026-03-07 19:23:50 +0800

    workflows

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ const config: Phaser.Types.Core.GameConfig = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    resolution: window.devicePixelRatio,
   },
 };
 
